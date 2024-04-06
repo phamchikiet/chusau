8c479aeab77e9ed4525df4d2cfdbe46774f577d2
chikiet88 <chikiet88@gmail.com>
2024-04-06 16:28:30 +0700

    update

--- apps/site/src/app/shared/telegram.service.ts
+++ apps/site/src/app/shared/telegram.service.ts
@@ -6,13 +6,6 @@ import axios from 'axios';
 })
 export class TelegramService {
   constructor() {}
-  // async SendNoti(data: string): Promise<any> {
-  //   const options = {
-  //     url: `https://api.telegram.org/bot${environment.APITelegram_accesstoken}/sendMessage?chat_id=${environment.APITelegram_idGroup}&text=${data}&parse_mode=html`,
-  //   };
-  //   const response = await axios.request(options);
-  //   return response.data;
-  // }
   async SendLogdev(data: any): Promise<any> {
     const options = {
       url: `https://api.telegram.org/bot${environment.APITelegram_accesstoken}/sendMessage?chat_id=${environment.APITelegram_Logdev}&text=${data}&parse_mode=html`,
